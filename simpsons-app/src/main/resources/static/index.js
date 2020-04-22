1cbfc93a3e743490b105b5d0887e664cd565836d
Álvaro Zarza Carrascal <alvaro.zarza@sngular.com>
2020-04-23 00:04:46 +0200

    Added first static version (Test of html and test JS).

---
+++ simpsons-app/src/main/resources/static/index.js
@@ -0,0 +1,15 @@
+
+$(document).on("click", "#buttonAddSimpson", function(){
+          console.log("CLICK");
+          $('#addModal').modal('show');
+});
+
+
+$(document).on("click", "#sendData", function(){
+           addSimpson();
+})
+
+function addSimpson() {
+
+    console.log("HELLLO");
+}
